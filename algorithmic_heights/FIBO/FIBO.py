ca92c353bb042ad8695423bd8e3176b66f797a30
Nick <nicksyz13@gmail.com>
2021-03-21 19:49:06 +0300

    Tail call: DONE

--- algorithmic_heights/FIBO/FIBO.py
+++ algorithmic_heights/FIBO/FIBO.py
@@ -1,11 +1,20 @@
 # http://rosalind.info/problems/list-view/?location=algorithmic-heights
 
-def fibonacci(element):
+# non-optimized variant
+def fibonacci_1(element):
     if element == 0:
         return 0
     if element == 1:
         return 1
-    return fibonacci(element-1) + fibonacci(element-2)
+    return fibonacci_1(element-1) + fibonacci_1(element-2)
+
+# version with a tail call (optimized variant)
+def fibonacci(element, next=1, prev=0):
+    if element == 0:
+        return prev
+    if element == 1:
+        return next
+    return fibonacci(element-1, next+prev, next)
 
 if __name__ == '__main__':
     with open('rosalind_fibo.txt') as f:
